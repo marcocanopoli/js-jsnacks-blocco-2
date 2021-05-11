cd5cb5e7c7d7ca6bcc8ef49ac46d0c176fc14c77
marcocanopoli <canopoli.marco@gmail.com>
2021-05-11 17:28:09 +0200

    range max value fix

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -4,23 +4,21 @@ Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più p
 La funzione restituirà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 */
 
-var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var numsCopy = copyArrayInRange(nums, 2, 7);
+var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numsCopy = copyArrayInRange(nums, 2, 9);
 console.log(numsCopy);
 
 //----- FUNCTIONS -----//
 //returns copy of array values included in specified range (a < b)
-function copyArrayInRange (array, a, b) {
+function copyArrayInRange(array, a, b) {
     var arrayCopy = [];
-    if (b > array.length){
-        alert("Range non valido: " + b + " e' maggiore della lunghezza dell'array (" + array.length + ")");
-    } else if (a > b ) {
+    if (b >= array.length){
+        alert("Range non valido: la posizione " + b + " nell'array di lunghezza " + array.length + " non esiste");
+    } else if (a > b) {
         alert("Range non valido: " + a + " e' maggiore di " + b);
     } else {
-        for (var i = 0; i < array.length; i++) {
-            if (i >= a && i <= b) {
-                arrayCopy.push(array[i]);
-            }
+        for (var i = a; i <= b; i++) {
+            arrayCopy.push(array[i]);
         }
     }
     return arrayCopy;
